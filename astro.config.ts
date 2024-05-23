7a8a86f977b5e3817efcce3ce5b9ab2727d2a4ea
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-23 06:34:19 +0300

    Filled out more info

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import { konamiEmojiBlast } from "@konami-emoji-blast/astro";
 
 export default defineConfig({
-	integrations: [konamiEmojiBlast()],
 	site: "https://squiggleconf.com",
 });
