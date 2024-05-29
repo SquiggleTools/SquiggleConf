4213cddc38d160e597d3d6cac7492730e9edfcb5
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-29 09:38:17 -0400

    analytics maybe

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,13 @@
+// vercel why are you not ok at this
+// @ts-ignore
+import vercel from "@astrojs/vercel/serverless";
+
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
 	site: "https://squiggleconf.com",
+	output: "server",
 });
