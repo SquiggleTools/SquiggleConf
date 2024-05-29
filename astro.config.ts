073839e7f6539bb450acaad3e5a86cbe6f1c7f3d
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-29 15:18:51 -0400

    fix og images

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://squiggleconf.com",
+	site: "https://2024.squiggleconf.com",
 	output: "server",
 	adapter: vercel({
 		webAnalytics: { enabled: true },
