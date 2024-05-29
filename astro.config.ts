a8ef6c9466e976a81025a59a081f046580100308
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-29 09:39:15 -0400

    npx astro add

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,10 @@
-// vercel why are you not ok at this
-// @ts-ignore
 import vercel from "@astrojs/vercel/serverless";
-
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	site: "https://squiggleconf.com",
+	output: "server",
 	adapter: vercel({
 		webAnalytics: { enabled: true },
 	}),
-	site: "https://squiggleconf.com",
-	output: "server",
 });
