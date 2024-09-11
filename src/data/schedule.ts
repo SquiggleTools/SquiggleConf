83a73dfb4d07e1f31294e30ec2c6a54d0b95965c
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-09-11 16:36:25 -0400

    feat: add speakers to schedule (#117)
    ## Overview
    
    Cross-references with the _sessions_ page too.
    
    💖

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -6,11 +6,28 @@ export interface ScheduleDay {
 }
 
 export interface ActivityData {
+	at?: string;
 	description: string[];
 	location?: ActivityLocation;
-	time?: string;
 	title: string;
+	within?: ActivityDataWithin[];
 }
+
+export interface ActivityDataWithinBase {
+	at: string;
+}
+
+export interface ActivityBetweenData extends ActivityDataWithinBase {
+	title: string;
+	type: "between";
+}
+
+export interface ActivitySessionData extends ActivityDataWithinBase {
+	session: string;
+	type: "session";
+}
+
+export type ActivityDataWithin = ActivityBetweenData | ActivitySessionData;
 
 export interface ActivityLocation {
 	href: string;
@@ -21,6 +38,7 @@ export const days: ScheduleDay[] = [
 	{
 		activities: [
 			{
+				at: "3:00pm - 6:00pm",
 				description: [
 					"[Artists for Humanity (AFH)](https://www.afhboston.org) is a Boston not-for-profit that employs local teens after school, giving them invaluable art, coding, and design experience.",
 					"We'll come over there to help the teens with their coding projects and run mini-workshops on useful development topics.",
@@ -30,7 +48,6 @@ export const days: ScheduleDay[] = [
 					href: "https://www.afhboston.org",
 					text: "Artists for Humanity EpiCenter",
 				},
-				time: "3:00pm - 6:00pm",
 				title: "Artists for Humanity Volunteering Event",
 			},
 		],
@@ -39,6 +56,7 @@ export const days: ScheduleDay[] = [
 	{
 		activities: [
 			{
+				at: "7:00am",
 				description: [
 					"Join organizers, volunteers, and attendees for a free joyful jog around the Boston harbor.",
 					"We'll meet at the New England Aquarium and run in a 5k loop at a comfortably light pace.",
@@ -47,10 +65,10 @@ export const days: ScheduleDay[] = [
 					href: "https://www.neaq.org/visit",
 					text: "New England Aquarium",
 				},
-				time: "7:00am",
 				title: "Morning Fun Run",
 			},
 			{
+				at: "8:30am",
 				description: [
 					"Come over to the venue, collect your badge, and network with fellow attendees.",
 				],
@@ -58,19 +76,56 @@ export const days: ScheduleDay[] = [
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "8:30am",
 				title: "Doors Open",
 			},
 			{
+				at: "9:40am",
 				description: ["Full-length and lightning talks from our speakers."],
 				location: {
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "9:40am",
 				title: "Talks",
+				within: [
+					{
+						at: "9:45am",
+						title: "Morning Announcements",
+						type: "between",
+					},
+					{
+						at: "9:55am",
+						session: "talk-robby-russell",
+						type: "session",
+					},
+					{
+						at: "10:30am",
+						session: "talk-chris-griffing",
+						type: "session",
+					},
+					{
+						at: "10:40am",
+						session: "talk-rizel-scarlett",
+						type: "session",
+					},
+					{
+						at: "11:10am",
+						title: "Short Break",
+						type: "between",
+					},
+					{
+						at: "11:30am",
+						session: "talk-don-isaac",
+						type: "session",
+					},
+					{
+						at: "12:00pm",
+						session: "talk-titian-cernicova-dragomir",
+						type: "session",
+					},
+				],
 			},
 			{
+				at: "12:30pm",
 				description: [
 					"Head over to the nearby Faneuil Hall Marketplace for lunch.",
 					"We'll have volunteers available to bring groups over to popular food establishments.",
@@ -79,19 +134,56 @@ export const days: ScheduleDay[] = [
 					href: "https://faneuilhallmarketplace.com",
 					text: "Faneuil Hall Marketplace",
 				},
-				time: "12:30pm",
 				title: "Lunch",
 			},
 			{
+				at: "2:00pm",
 				description: ["Full-length and lightning talks from our speakers."],
 				location: {
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "2:00pm",
 				title: "Talks",
+				within: [
+					{
+						at: "2:00pm",
+						session: "talk-laurynas-keturakis",
+						type: "session",
+					},
+					{
+						at: "2:10pm",
+						session: "talk-aakansha-doshi",
+						type: "session",
+					},
+					{
+						at: "2:40pm",
+						session: "talk-corina-murg",
+						type: "session",
+					},
+					{
+						at: "2:50pm",
+						session: "talk-loris-cro",
+						type: "session",
+					},
+					{
+						at: "3:20pm",
+						title: "Short Break",
+						type: "between",
+					},
+					{
+						at: "3:45pm",
+						session: "talk-jess-lin",
+						type: "session",
+					},
+					{
+						at: "4:15pm",
+						session: "talk-yagiz-nizipli",
+						type: "session",
+					},
+				],
 			},
 			{
+				at: "4:30pm",
 				description: [
 					"What a day! Let's take a breather to have a snack and chat.",
 				],
@@ -99,19 +191,36 @@ export const days: ScheduleDay[] = [
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "4:30pm",
 				title: "Afternoon Snack",
 			},
 			{
+				at: "5:00pm",
 				description: ["Full-length and lightning talks from our speakers."],
 				location: {
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "5:00pm",
 				title: "Talks",
+				within: [
+					{
+						at: "5:00pm",
+						session: "talk-rick-viscomi",
+						type: "session",
+					},
+					{
+						at: "5:30pm",
+						session: "talk-rachel-nabors",
+						type: "session",
+					},
+					{
+						at: "5:40pm",
+						session: "talk-dan-vanderkam",
+						type: "session",
+					},
+				],
 			},
 			{
+				at: "6:15pm",
 				description: [
 					"Final pieces of information on upcoming events, raffle giveaways, and appreciation notes to all of the lovely people who attended.",
 				],
@@ -119,7 +228,6 @@ export const days: ScheduleDay[] = [
 					href: "https://www.neaq.org/visit/simons-theatre",
 					text: "Simons Theater",
 				},
-				time: "6:15pm",
 				title: "Closing Announcements",
 			},
 			{
@@ -135,6 +243,7 @@ export const days: ScheduleDay[] = [
 	{
 		activities: [
 			{
+				at: "7:00am",
 				description: [
 					"Join organizers, volunteers, and attendees for a free joyful jog around the Boston harbor.",
 					"We'll meet at the New England Aquarium and run in a 5k loop at a comfortably light pace.",
@@ -143,14 +252,26 @@ export const days: ScheduleDay[] = [
 					href: "https://www.neaq.org/visit",
 					text: "New England Aquarium",
 				},
-				time: "7:00am",
 				title: "Morning Fun Run",
 			},
 			{
+				at: "8:30am",
 				description: [
 					"Learn actionable day-to-day skills and technological takeaways from some of our partners and sponsors.",
 				],
 				title: "Morning Workshops",
+				within: [
+					{
+						at: "8:30am",
+						session: "workshop-katerina-and-max",
+						type: "session",
+					},
+					{
+						at: "10:30am",
+						title: "Workshop to be announced",
+						type: "between",
+					},
+				],
 			},
 		],
 		title: "Friday, October 4th",
