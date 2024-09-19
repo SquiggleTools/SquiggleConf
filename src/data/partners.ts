0e4504e7437d7e9979f363f94c41458da37cd9d6
Josh Goldberg <git@joshuakgoldberg.com>
2024-09-19 13:38:05 -0400

    fix: remove preemptively added Hack.Diversity for now

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,8 +1,6 @@
 import afhBlack from "../assets/partners/afh-black.svg";
 import afhWhite from "../assets/partners/afh-white.svg";
 import conTejasCode from "../assets/partners/contejas-code.png";
-import hackDiversityBlue from "../assets/partners/hack-diversity-blue.png";
-import hackDiversityWhite from "../assets/partners/hack-diversity-white.png";
 import jsParty from "../assets/partners/js-party.png";
 import thisWeekInReactDarkMode from "../assets/partners/this-week-in-react-dark-mode.png";
 import thisWeekInReactLightMode from "../assets/partners/this-week-in-react-light-mode.png";
@@ -23,14 +21,6 @@ export const partnersBySlug = {
 			light: conTejasCode,
 		},
 		title: "ConTejas Code Podcast",
-	},
-	hackDiversity: {
-		href: "https://www.hackdiversity.com/?utm_source=squiggleconf",
-		logos: {
-			dark: hackDiversityWhite,
-			light: hackDiversityBlue,
-		},
-		title: "Hack.Diversity",
 	},
 	jsparty: {
 		href: "https://changelog.com/jsparty?utm_source=squiggleconf",
