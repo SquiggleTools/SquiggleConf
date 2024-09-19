0c3379c3ed6b626ee913499b38942a9d39e332b1
Josh Goldberg <git@joshuakgoldberg.com>
2024-09-19 13:43:33 -0400

    fix: use proper svg assets for Hack.Diversity

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -1,6 +1,8 @@
 import afhBlack from "../assets/partners/afh-black.svg";
 import afhWhite from "../assets/partners/afh-white.svg";
 import conTejasCode from "../assets/partners/contejas-code.png";
+import hackDiversityBlue from "../assets/partners/hack-diversity-blue.svg";
+import hackDiversityWhite from "../assets/partners/hack-diversity-white.svg";
 import jsParty from "../assets/partners/js-party.png";
 import thisWeekInReactDarkMode from "../assets/partners/this-week-in-react-dark-mode.png";
 import thisWeekInReactLightMode from "../assets/partners/this-week-in-react-light-mode.png";
@@ -21,6 +23,14 @@ export const partnersBySlug = {
 			light: conTejasCode,
 		},
 		title: "ConTejas Code Podcast",
+	},
+	hackDiversity: {
+		href: "https://www.hackdiversity.com/?utm_source=squiggleconf",
+		logos: {
+			dark: hackDiversityWhite,
+			light: hackDiversityBlue,
+		},
+		title: "Hack.Diversity",
 	},
 	jsparty: {
 		href: "https://changelog.com/jsparty?utm_source=squiggleconf",
