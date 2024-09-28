507232dbc92cffe2f2dc2f526911d27a90c8b044
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-09-28 09:52:00 -0400

    feat: add link to shop (#127)
    💖

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,6 @@
 export const links = {
 	scholarship: "https://forms.gle/pnoAmVgijk3p4j5C7",
+	shop: "https://shop.squiggle.tools",
 	tickets: "https://ticket.squiggleconf.com",
 	volunteer: "https://volunteer.squiggleconf.com",
 	volunteering: "https://forms.gle/EhSEqs3HD5QsFhCz9",
