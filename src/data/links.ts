eaa3ab0a2645f70ff3e205fdaf25e1de80262355
Josh Goldberg <git@joshuakgoldberg.com>
2024-11-30 20:04:21 -0500

    feat: add 2025 links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,5 @@
 export const links = {
+	nextYear: "https://2025.squiggleconf.com",
 	scholarship: "https://forms.gle/pnoAmVgijk3p4j5C7",
 	shop: "https://shop.squiggle.tools",
 	tickets: "https://ticket.squiggleconf.com",
